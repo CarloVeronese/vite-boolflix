f9f63658eaeb695eefc44fe58e5f6b2650aad5a0
CarloVeronese <carlo.veronese97@gmail.com>
2023-11-10 16:26:37 +0100

    Add - search function with emit and api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script>
 import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import {store} from './store'
 export default {
-  created() {
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=2e97dd65b2da08753e19493d18e36c44&query=ritorno+al+futuro')
-    .then(res => {
-      console.log(res);
-    })
+  data() {
+    return {
+      store: store,
+    }
   },
+  components: {
+    AppHeader
+  },
+  methods: {
+    searchTitle() {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=2e97dd65b2da08753e19493d18e36c44&query=${this.store.stringSearched}`)
+      .then(res => {
+        console.log(res.data)
+      })
+    }
+  }
 }
 </script>
 
 <template>
+  <AppHeader @performSearch="searchTitle"/>
 </template>
 
-<style scoped>
+<style>
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+import { store } from '../store'
+export default {
+    data() {
+        return {
+            store: store,
+        }
+    },
+    methods: {
+        callSearch() {
+            this.$emit('performSearch')
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="search-bar">
+            <input type="text" v-model="store.stringSearched">
+            <button @click="callSearch">Search</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .search-bar {
+        display: flex;
+    }
+</style>
