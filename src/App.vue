81fe35ed09c7dd8c1e9eafd38fadbe40c69e1643
CarloVeronese <carlo.veronese97@gmail.com>
2023-11-10 15:44:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from 'axios';
+export default {
+  created() {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=2e97dd65b2da08753e19493d18e36c44&query=ritorno+al+futuro')
+    .then(res => {
+      console.log(res);
+    })
+  },
+}
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
